d8f1ae576f8bd7400b4ba0b91aa70d0afbbdbaaf
JaiBhalla03 <jai.bhalla2021@vitstudent.ac.in>
2024-03-09 17:25:30 +0530

    more codes

---
+++ backend/mindmatrix/src/main/java/com/example/mindmatrix/MindmatrixApplication.java
@@ -0,0 +1,13 @@
+package com.example.mindmatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindmatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindmatrixApplication.class, args);
+	}
+
+}
